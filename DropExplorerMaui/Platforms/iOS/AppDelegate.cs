e196295efcd5c4560fd2b83263bb44780b0e168c
Peter Spada <spadapet@hotmail.com>
2022-06-20 15:38:21 -0700

    Switch to single project

---
+++ DropExplorerMaui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace DropExplorerMaui;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
