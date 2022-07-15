86d4aefed9a98acb7322322e37aad843d1af5f70
Peter Spada <spadapet@hotmail.com>
2022-07-15 09:10:27 -0700

    Start UI

--- DropExplorerMaui/App.xaml.cs
+++ DropExplorerMaui/App.xaml.cs
@@ -2,10 +2,8 @@
 
 public partial class App : Application
 {
-	public App()
-	{
-		InitializeComponent();
-
-		MainPage = new AppShell();
-	}
+    public App()
+    {
+        InitializeComponent();
+    }
 }
--- DropExplorerMaui/MauiProgram.cs
+++ DropExplorerMaui/MauiProgram.cs
@@ -4,15 +4,13 @@ public static class MauiProgram
 {
 	public static MauiApp CreateMauiApp()
 	{
-		var builder = MauiApp.CreateBuilder();
-		builder
+		return MauiApp.CreateBuilder()
 			.UseMauiApp<App>()
 			.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
-
-		return builder.Build();
+			})
+			.Build();
 	}
 }
